a1a175df25ae3ef9bb7224cbe7bed1675d67e5e1
atreyamj <atreyamj@usc.edu>
2016-09-17 16:55:23 -0700

    Added methods to read from JSON, add-one smoothing

--- nbclassify.py
+++ nbclassify.py
@@ -1,6 +1,8 @@
 import os
 import sys
-
+import json
+import math
+jsonModel={}
 spamDicts={}
 hamDicts={}
 spamProb=0.0
@@ -8,49 +10,24 @@ hamProb=0.0
 
 def buildModel(modelName):
     global spamProb,hamProb
-    isSpam=0
-    isHam=0
+    global spamDicts,hamDicts,jsonModel
     with open(modelName, 'r', encoding="latin1") as modelFile:
-        lineCount=1
-        for line in modelFile:
-            for word in line.split("="):
-                word = word.rstrip('\n').rstrip('\r')
-                if lineCount == 1:
-                    spamProb=float(word)
-                elif lineCount == 2:
-                    hamProb=float(word)
-                else:
-                    if word=="SPAM":
-                        isSpam=1
-                        isHam=0
-                    elif word=="HAM":
-                        isSpam=0
-                        isHam=1
-                    else:
-                        tokenIndex=0
-                        tokens=line.split(" ")
-                        if(len(line.split(" "))==1):
-                            if isSpam==1: # Not Required
-                                spamDicts[" "]=float(line.lstrip("=").rstrip("\r").rstrip("\n"))
-                            elif isHam==1:
-                                hamDicts[" "]=float(line.lstrip("=").rstrip("\r").rstrip("\n"))
-                        elif(len(tokens)==2):
-                            if isSpam==1:
-                                spamDicts[str(tokens[tokenIndex])]=float(tokens[tokenIndex+1])
-                            elif isHam==1:
-                                hamDicts[str(tokens[tokenIndex])]=float(tokens[tokenIndex+1])
-                lineCount+=1
+        jsonModel=json.load(modelFile)
+    spamDicts= jsonModel["SPAM"]
+    hamDicts = jsonModel["HAM"]
+    spamProb= float(math.log10((jsonModel["spamFileCount"]/jsonModel["filesTotal"])))
+    hamProb = float(math.log10((jsonModel["hamFileCount"] / jsonModel["filesTotal"])))
 
 def getSpamProbability(word):
-    global spamDicts
+    global spamDicts,jsonModel
     if word in spamDicts:
-        return spamDicts[word]
+        return float(math.log10(((spamDicts[word]+1))/(jsonModel["spamWordTotal"]+jsonModel["uniqueWords"])))
     return 0
 
 def getHamProbability(word):
     global hamDicts
     if word in hamDicts:
-        return hamDicts[word]
+        return float(math.log10(((hamDicts[word]+1)/(jsonModel["hamWordTotal"]+jsonModel["uniqueWords"]))))
     return 0
 
 def doClassifyDocument(fileName):
@@ -61,8 +38,8 @@ def doClassifyDocument(fileName):
         for line in f:
             for word in line.split(" "):
                 word=word.rstrip('\n').rstrip('\r')
-                spamProbab+=getSpamProbability(word)
-                hamProbab+=getHamProbability(word)
+                spamProbab=spamProbab+getSpamProbability(word)
+                hamProbab=hamProbab+getHamProbability(word)
         spamProbab+=spamProb
         hamProbab+=hamProb
         if spamProbab > hamProbab:
@@ -73,18 +50,29 @@ def doClassifyDocument(fileName):
             return 2
 
 def getClassification(directoryPath):
+    correctSpams=0
+    correctHams=0
+    classifiedSpams=0
+    classifiedHams=0
     for root, dirs, files in os.walk(directoryPath):
         path = root.split('/')
         for file in files:
             if file.endswith(".txt"):
                 classifcationValue=doClassifyDocument(os.path.join(root,file))
                 if classifcationValue ==0:
+                    if file.find("spam")!=-1:
+                        correctSpams+=1
+                    classifiedSpams+=1
                     print(file + " - SPAM")
                 elif classifcationValue ==1:
+                    if file.find("ham")!=-1:
+                        correctHams+=1
+                    classifiedHams+=1
                     print(file + " - HAM")
                 else:
                     print(file + " - Can't Classify")
-
+    #print("SPAM: "+str(correctSpams/classifiedSpams))
+    #print("HAM: " + str(correctHams/ classifiedHams))
 if  len(sys.argv) != 2:
     print("Error: The input data path is NULL or empty\n")
     sys.exit(-1)
